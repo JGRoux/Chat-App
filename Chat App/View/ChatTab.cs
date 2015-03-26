ab10d8947d5178c4097a75cf336f212d29c87080
alexandrebensimon <alexandrebensimon@free.fr>
2015-03-26 02:03:23 +0100

    Commit de 2h00... Cette socket exception commence à me les briser !

--- Chat App/View/ChatTab.cs
+++ Chat App/View/ChatTab.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Chat_Client.Model;
 using Chat_Library.Model;
+using System.Net.Sockets;
+using Chat_Library.Controller;
+using System.Net;
 
 namespace Chat_Client
 {
@@ -21,13 +24,24 @@ namespace Chat_Client
             InitializeComponent();
             this.client = client;
 
+            Console.WriteLine("URI: " + this.client.Channel.Uri);
+            Socket soso = new Socket(AddressFamily.InterNetwork,
+                             SocketType.Stream,
+                             ProtocolType.Tcp);
+            soso.Bind(new IPEndPoint(IPAddress.Parse(this.client.Channel.Uri), this.client.Channel.Port));
+            soso.Connect(this.client.Channel.Uri, this.client.Channel.Port);
+
+            /*this.client.Connection = new Connection(new Socket(AddressFamily.InterNetwork,
+                             SocketType.Stream,
+                             ProtocolType.Tcp));
+
             this.client.Connection.bind(this.client.Channel.Uri, this.client.Channel.Port);
-            this.client.Connection.connect(this.client.Channel.Uri, this.client.Channel.Port);
+            this.client.Connection.connect(this.client.Channel.Uri, this.client.Channel.Port);*/
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
